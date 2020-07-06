64ec287eea1db9a49b58dfc8bd7c759c08c731d6
juanb86 <juan.e.begnis@gmail.com>
2020-07-06 20:14:27 -0300

    Updates

---
+++ precache-manifest.b9b277bcc62a503c42457abcc1bd0cac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7de829f71580d9d4bbfc7bb89bc8ad62",
+    "url": "/pomodoro_clock/index.html"
+  },
+  {
+    "revision": "00492f1e749b9ac40eae",
+    "url": "/pomodoro_clock/static/css/main.7b11914d.chunk.css"
+  },
+  {
+    "revision": "58e9435124d9ce81d166",
+    "url": "/pomodoro_clock/static/js/2.0b3feac1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pomodoro_clock/static/js/2.0b3feac1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00492f1e749b9ac40eae",
+    "url": "/pomodoro_clock/static/js/main.e36c16f0.chunk.js"
+  },
+  {
+    "revision": "876221ed443e52accde0",
+    "url": "/pomodoro_clock/static/js/runtime-main.e6f5db23.js"
+  },
+  {
+    "revision": "2d027bca13d20a4d52e8746ab872de91",
+    "url": "/pomodoro_clock/static/media/buzzer.2d027bca.mp3"
+  }
+]);
