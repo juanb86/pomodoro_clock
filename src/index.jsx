18ae90ad86f3fc04fd18832cc3f0ccd32c3ee5b5
juanb86 <juan.e.begnis@gmail.com>
2020-07-09 12:58:01 -0300

    Complete timer section styling

--- src/index.jsx
+++ src/index.jsx
@@ -38,6 +38,33 @@ function Time(props) {
     <div id="time">
       <div id="inner-border">
         <h3 id="timer-label">{props.fase}</h3>
+        <div id="label-session">
+          <h3>Session</h3>
+          <h3
+            style={
+              props.fase === "Session"
+                ? { display: "initial" }
+                : { display: "none" }
+            }
+            className="fase-dot"
+          >
+            .
+          </h3>
+        </div>
+        <div id="label-break">
+          <h3>Break</h3>
+          <h3
+            style={
+              props.fase === "Break"
+                ? { display: "initial" }
+                : { display: "none" }
+            }
+            className="fase-dot"
+          >
+            .
+          </h3>
+        </div>
+
         <div id="time-left">{props.time}</div>
         <div id="div-start_stop">
           <button id="start_stop" onClick={props.updateTime} />
